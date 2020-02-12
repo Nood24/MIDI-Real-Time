6cb685e16f5c2d75fade9f3b2ee9d0c37deb9ce1
Nood24 <neilwood1997@gmail.com>
2020-02-12 16:49:26 +0000

    Update buttonPressDetect.cpp

--- HardwareTests/buttonPressDetect.cpp
+++ HardwareTests/buttonPressDetect.cpp
@@ -1,33 +1,26 @@
-import RPi.GPIO as GPIO
-import time
-import subprocess, os
-import signal
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO_switch = 24  # pin 18
-GPIO.setup(GPIO_switch,GPIO.IN)
+#include <stdio.h>
+#include <wiringPi.h>
 
-print "  Press Ctrl & C to Quit"
+int main(int argc, char** argv)
+{
+    // Intialize the wiringPi Library
+    wiringPiSetup();
 
-try:
-    
-   run = 0
-   while True :
-      if GPIO.input(GPIO_switch)==0 and run == 0:
-         print "  Started"
-         rpistr = "FilePathExample; ./executable"
-         p=subprocess.Popen(rpistr,shell=True, preexec_fn=os.setsid)
-         run = 1
-         while GPIO.input(GPIO_switch)==0:
-             time.sleep(0.1)
-      if GPIO.input(GPIO_switch)==0 and run == 1:
-         print "  Stopped " 
-         run = 0
-         os.killpg(p.pid, signal.SIGTERM)
-         while GPIO.input(GPIO_switch)==0:
-             time.sleep(0.1)
-       
+	const int INPUT_PIN = 12;
+	
+    // Read input on this pin
+    pinMode(INPUT_PIN, INPUT);
 
-except KeyboardInterrupt:
-  print "  Quit"
-  GPIO.cleanup()
+    while(true)
+    {
+        // As soon as we dedect an input, log and quit.
+        if(digitalRead(INPUT_PIN) == HIGH)
+		{
+			printf("Button is pressed!\n");	
+			break;
+		}
+    }
+
+    // Exit program
+    return 0;
+}
