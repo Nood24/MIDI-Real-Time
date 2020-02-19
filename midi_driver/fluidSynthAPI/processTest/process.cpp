599757762777281e3106cf1104a160e86f3b9661
Neil <neilwood1997@gmail.com>
2020-02-19 09:52:18 -0500

    Progress in triggering on off notes

--- midi_driver/fluidSynthAPI/processTest/process.cpp
+++ midi_driver/fluidSynthAPI/processTest/process.cpp
@@ -10,11 +10,21 @@
 #include <iostream>
 #include <cstdlib>
 #include <iostream>
-
+#include <bitset>
 
 using namespace std;
 
 std::vector<unsigned char>* message;
+
+
+bool isKthBitSet(int n, int k) 
+{ 
+    if ((n >> (k - 1)) & 1) 
+        return true;
+    else
+        return false; 
+} 
+ 
 
 void setMessage(std::vector<unsigned char>* newMessage){
  message = newMessage;
@@ -22,8 +32,23 @@ void setMessage(std::vector<unsigned char>* newMessage){
  int byte1 =  message->at(0);
  int byte2 = message->at(1);
  int byte3 = message->at(2);
- printf("Printing MIDI Message %d %d %d \n", byte1,byte2,byte3);
+ char result = byte1 && 00010000;
+
+ bool is_set = isKthBitSet(byte1,5);
+
+
+
+//std::cout << res;
+if( is_set){
+ printf("true\n");
+}
+else{
+ printf("false\n");
+}
+
+ //printf("Number of bytes %d Printing MIDI Message %d %d %d \n",nBytes, byte1,byte2,byte3);
  fluid_synth_init();
+
  noteOn(0,60);
   
  
