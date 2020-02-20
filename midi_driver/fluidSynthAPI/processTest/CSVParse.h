f64f756cd2a55e4364a45ee1196dce1b5f53eff8
Bennett1709 <2193356B@student.gla.ac.uk>
2020-02-20 15:52:05 +0000

    MIDI editing

---
+++ midi_driver/fluidSynthAPI/processTest/CSVParse.h
@@ -0,0 +1,13 @@
+//
+// Created by Cameron Bennett on 19/02/2020.
+//
+
+#ifndef MIDI_REAL_TIME_CSVPARSE_H
+#define MIDI_REAL_TIME_CSVPARSE_H
+
+#endif //MIDI_REAL_TIME_CSVPARSE_H
+using namespace std;
+
+void loadCSVToArray(string &filename, vector<int> &timeDeltas, vector<int> &channels, vector<int> &onOff);
+
+void *Play(void *arraySize);
