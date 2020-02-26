61524509b183d3976be445b33d4b82af66733626
Neil Wood <neilwood1997@gmail.com>
2020-02-26 18:16:00 +0000

    Hardware code for testing GPIO Footpedals

---
+++ HardwareTests/GPIO_MultiplePushbuttonLED.cpp
@@ -0,0 +1,94 @@
+#include<iostream>  //Include all needed libraries
+#include<wiringPi.h>   //Add the wiringPi library to control GPIO pins on C++
+
+using namespace std;
+
+int main()
+{	//Declare the GPIO pins for each system component
+
+// Switch1: Physical pin 12, wiringPi 1, BCM GPIO18.
+
+const int button1 = 1;
+
+//LED1: Physical pin 11, wiringPi 0, BCM GPIO17.
+const int light1 = 0; 
+
+// Switch2: Physical pin 16, wiringPi 4, BCM GPIO23.
+
+const int button2 = 4;
+
+//LED2: Physical pin 15, wiringPi 3, BCM GPIO22.
+const int light2 = 3; 
+
+// Switch3: Physical pin 18, wiringPi 5, BCM GPIO24.
+
+const int button3 = 5;
+
+//LED3: Physical pin 19, wiringPi 12, BCM GPIO10.
+const int light3 = 12; 
+
+// Switch4: Physical pin 22, wiringPi 6, BCM GPIO25.
+
+const int button4 = 6;
+
+//LED4: Physical pin 21, wiringPi 13, BCM GPIO9.
+const int light4 = 13; 
+
+
+wiringPiSetup();      //Setup the Pi Library
+pinMode(button1, INPUT);   //Set button1 as an input
+pinMode(light1, OUTPUT);   //Set LED1 as an output
+
+pinMode(button2, INPUT);   //Set button2 as an input
+pinMode(light2, OUTPUT);   //Set LED2 as an output
+
+pinMode(button3, INPUT);   //Set button3 as an input
+pinMode(light3, OUTPUT);   //Set LED3 as an output
+
+pinMode(button4, INPUT);   //Set button4 as an input
+pinMode(light4, OUTPUT);   //Set LED4 as an output
+
+while(1)
+{	
+	//When button1 is pressed digitalRead returns LOW
+	if (digitalRead(button1) == LOW)
+{	
+	printf("Button1 pressed\n");
+	//Turn on LED1
+	digitalWrite(light1, HIGH);
+} else {
+	digitalWrite(light1, LOW);
+}
+//When button2 is pressed digitalRead returns LOW
+	if (digitalRead(button2) == LOW)
+{	
+	printf("Button2 pressed\n");
+	//Turn on LED2
+	digitalWrite(light2, HIGH);
+} else {
+	digitalWrite(light2, LOW);
+}
+
+//When button3 is pressed digitalRead returns LOW
+	if (digitalRead(button3) == LOW)
+{	
+	printf("Button3 pressed\n");
+	//Turn on LED3
+	digitalWrite(light3, HIGH);
+} else {
+	digitalWrite(light3, LOW);
+}
+
+//When button4 is pressed digitalRead returns LOW
+	if (digitalRead(button4) == LOW)
+{	
+	printf("Button4 pressed\n");
+	//Turn on LED4
+	digitalWrite(light4, HIGH);
+} else {
+	digitalWrite(light4, LOW);
+}
+
+}
+return 0; 
+}
