4e31dede73c1492b246bd8391af916a2ae92b01f
Neil <neilwood1997@gmail.com>
2020-02-26 09:34:44 -0500

    tidy up process.cpp

--- midi_driver/fluidSynthAPI/processTest/process.h
+++ midi_driver/fluidSynthAPI/processTest/process.h
@@ -1,17 +1,16 @@
 
+class Process{
 
-class Process
-{
-private:
+  private:
     std::vector<unsigned char>* message;
-    int chordNotes[3] = {60,64,67}; //CHANGE
-    int chordIdx = 0;
-    int bassNote = 48; //CHANGE
+    int chordNotes[3]; //CHANGE
+    int chordIdx;
+    int bassNote; //CHANGE
     int byte1;
     int byte2;
     unsigned int nBytes;
     bool is_bass;
-    int tempo = 650;
+    int tempo;
     std::vector<int> timeDeltas, channels, onOff;
     //bool is_set;
 
@@ -23,12 +22,8 @@ private:
 
     void loadCSVToArray(std::string &filename, std::vector<int> &timeDeltas, std::vector<int> &channels, std::vector<int> &onOff);
 
-    void sendNote(bool on, int channel, int note);
-
-
-
  
-public:
+  public:
     void setMessage(std::vector<unsigned char>* setMessage);
 
     void printMessage();
