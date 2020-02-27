2e90ecf0f8e6321333276b2938d81cacf98eff0a
Neil <neilwood1997@gmail.com>
2020-02-27 14:11:06 -0500

    Hardware Controller Framework

--- hardwareController/HardwareController.h
+++ hardwareController/HardwareController.h
@@ -1,36 +1,23 @@
+//*****************************************//
+//  
+//  HardwareController.h
+//  Class Interfacing with hardware and
+//  maintaining system state
+//
+//*****************************************//
 
-class Process{
+
+class HardwareController{
 
   private:
-    std::vector<unsigned char>* message;
-    int chordNotes[3]; //CHANGE
-    int chordIdx;
-    int bassNote; //CHANGE
-    int byte1;
-    int byte2;
-    unsigned int nBytes;
-    bool is_bass;
-    int tempo;
-    std::vector<int> timeDeltas, channels, onOff;
-    //bool is_set;
 
-    bool isKthBitSet(int n, int k);
-    
-    void setChordNote(int note);
+bool songPlaying;
+char songs [4][30];
 
-    int getChordNotes();
-
-    void loadCSVToArray(std::string &filename, std::vector<int> &timeDeltas, std::vector<int> &channels, std::vector<int> &onOff);
-
- 
   public:
-    void setMessage(std::vector<unsigned char>* setMessage);
-
-    void printMessage();
-
     void run();
 
-    void *Play(void *arraySize);
+
 };
  
 
---
+++ hardwareController/HardwareController_Test.cpp
@@ -0,0 +1,7 @@
+#include "HardwareController.h"
+
+HardwareController controller1;
+
+int main(){
+	controller1.run();
+}
