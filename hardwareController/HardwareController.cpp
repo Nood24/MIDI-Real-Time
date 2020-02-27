09b3cfbbab1f6158f927cf43e5fb43fb366d1ff4
Neil <neilwood1997@gmail.com>
2020-02-27 14:11:06 -0500

    Hardware Controller Framework

--- hardwareController/HardwareController.cpp
+++ hardwareController/HardwareController.cpp
@@ -5,5 +5,28 @@
 //  maintaining system state
 //
 //*****************************************//
+#include "HardwareController.h"
+#include <iostream>
+using namespace std;
+
+bool songPlaying = false;
+char songs [4][30] = {"The Gay Gordons", "The Dashing White Sergeant", "Canadian Barn Dance","Highland Schottishe" }; 
 
 
+
+int getInput(){
+    int i;
+    cout << "Please enter an integer value between 1-4 for pedal press: ";
+    cin >> i;
+    cout << "The value you entered is " << i << "\n";
+    return i;
+}
+
+
+
+void HardwareController::run(){
+    while(true){
+    int input = getInput();
+    }
+
+}
