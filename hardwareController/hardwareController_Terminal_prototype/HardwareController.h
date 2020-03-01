52131731c77b6618a0a93906d6e1a858b23cc4d0
Neil <neilwood1997@gmail.com>
2020-03-01 11:09:55 -0500

    Storing controller prototype

---
+++ hardwareController/hardwareController_Terminal_prototype/HardwareController.h
@@ -0,0 +1,16 @@
+//*****************************************//
+//  
+//  HardwareController.h
+//  Class Interfacing with hardware and
+//  maintaining system state
+//
+//*****************************************//
+#include <string>
+    void run();
+    bool IsSongPlaying();
+    char * getSong();
+
+
+ 
+
+
