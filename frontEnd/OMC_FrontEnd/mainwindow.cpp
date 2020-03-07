0de824e1d2a9d389c8933a6e10de4f042a639ff6
Neil <neilwood1997@gmail.com>
2020-03-07 16:38:20 -0500

    Adding left right and play pause.

--- frontEnd/OMC_FrontEnd/mainwindow.cpp
+++ frontEnd/OMC_FrontEnd/mainwindow.cpp
@@ -7,21 +7,32 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     QPixmap pix(":/images/images/OneManCeilidh.jpg");
-    int w = this->width();
+    int w = ui->OMC_logo->width();
     int h = ui->OMC_logo->height();
     ui->OMC_logo->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
 
     QPixmap pix2(":/images/images/OneManCeilidh2.jpg");
-    int w_2 = ui->OMC_logo_2->width();
-    int h_2 = ui->OMC_logo_2->height();
-    ui->OMC_logo_2->setPixmap(pix2.scaled(w_2,h_2,Qt::KeepAspectRatio));
+    ui->OMC_logo_2->setPixmap(pix2.scaled(w,h,Qt::KeepAspectRatio));
 
     QFont font = ui->song_status->font();
     font.setPointSize(24);
     ui->song_status->setFont(font);
     ui->song_status->setText("The Current Song is: \n\n\nTemp Song Name!");
 
+    font.setPointSize(16);
+    ui->left_song->setFont(font);
+    ui->left_song->setText("Switch Left For: \n\nTemp Song Name!");
 
+    font.setPointSize(16);
+    ui->right_song->setFont(font);
+    ui->right_song->setText("Switch Right For: \n\nTemp Song Name!");
+
+    font.setPointSize(16);
+    ui->play_state->setFont(font);
+    ui->play_state->setText("Song Is Currently Playing!\n");
+
+    QPixmap pix3(":/images/images/greenButton.jpg");
+    ui->playButton->setPixmap(pix3.scaled(w*1.3,h*1.3,Qt::KeepAspectRatio));
 
     this->setWindowTitle(
             QApplication::translate("toplevel", "One Man Ceilidh"));
