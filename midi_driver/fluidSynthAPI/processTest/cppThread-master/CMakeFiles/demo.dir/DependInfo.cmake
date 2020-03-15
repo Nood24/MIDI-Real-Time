6ba94cfdd9a03ed5754687614f3cb3954bc66360
Bennett1709 <2193356b@student.gla.ac.uk>
2020-03-15 20:06:29 +0000

    C++ Threads library added

---
+++ midi_driver/fluidSynthAPI/processTest/cppThread-master/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/cameronbennett/Desktop/CeilidhBand/midi_driver/fluidSynthAPI/processTest/cppThread-master/DemoThread.cpp" "/Users/cameronbennett/Desktop/CeilidhBand/midi_driver/fluidSynthAPI/processTest/cppThread-master/CMakeFiles/demo.dir/DemoThread.cpp.o"
+  "/Users/cameronbennett/Desktop/CeilidhBand/midi_driver/fluidSynthAPI/processTest/cppThread-master/demo.cpp" "/Users/cameronbennett/Desktop/CeilidhBand/midi_driver/fluidSynthAPI/processTest/cppThread-master/CMakeFiles/demo.dir/demo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
