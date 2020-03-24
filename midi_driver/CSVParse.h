df24065a56ac20fffcdaa6defc0c1853ea0b4b91
Neil <neilwood1997@gmail.com>
2020-03-24 10:28:45 -0400

    Removing old Midi development files

--- midi_driver/CSVParse.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Cameron Bennett on 19/02/2020.
-//
-
-#ifndef MIDI_REAL_TIME_CSVPARSE_H
-#define MIDI_REAL_TIME_CSVPARSE_H
-
-#endif //MIDI_REAL_TIME_CSVPARSE_H
--- midi_driver/start_fluid_synth.sh
+++
@@ -1,1 +0,0 @@
-fluidsynth -a alsa -m alsa_seq -g 1.5 -s -c 4 -z 64 -r 44100  /usr/share/sounds/sf2/FluidR3_GM.sf2
